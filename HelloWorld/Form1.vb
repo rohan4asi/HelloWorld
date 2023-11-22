7de0c0b476dca9e2b3a17af2be20c87daa7eed7e
rohan4asi <rohan@anandsystems.com>
2023-11-22 09:41:17 +0530

    Initial commit

---
+++ HelloWorld/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Hello World...!"
+        Application.DoEvents()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Message to Display"
+        Application.DoEvents()
+
+
+
+    End Sub
+End Class
